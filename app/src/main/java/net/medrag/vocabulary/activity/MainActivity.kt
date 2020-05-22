b22b18a0852ebaffd3fd9bb00f18d6c13e279d92
WildCoder <fg2946gznal30fu2>
2020-05-23 01:43:27 +0300

    version 1.1.1 - clipboard

--- app/src/main/java/net/medrag/vocabulary/activity/MainActivity.kt
+++ app/src/main/java/net/medrag/vocabulary/activity/MainActivity.kt
@@ -1,5 +1,8 @@
 package net.medrag.vocabulary.activity
 
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
@@ -7,6 +10,7 @@ import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main.*
 import net.medrag.vocabulary.R
 import net.medrag.vocabulary.db.ReducedPair
@@ -39,6 +43,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun pasteFromClipboardEn(@Suppress("UNUSED_PARAMETER") view: View) = pasteFromClipboard(wilEdit)
+    fun pasteFromClipboardRu(@Suppress("UNUSED_PARAMETER") view: View) = pasteFromClipboard(tilEdit)
+
+    private fun pasteFromClipboard(inputField: TextInputEditText) {
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        if (clipboard.hasPrimaryClip()) {
+            val clip = clipboard.primaryClipDescription
+            if (clip?.hasMimeType(MIMETYPE_TEXT_PLAIN) == true) {
+                inputField.setText(clipboard.primaryClip?.getItemAt(0)?.text)
+            } else showToast("Not a text in the clipboard!")
+        } else showToast("The clipboard is empty!")
+    }
+
     fun dump(@Suppress("UNUSED_PARAMETER") view: View) {
         val dump: List<String> =
             database.extractAll().map { ReducedPair(it.trans, it.word) }.map { it.toString() }
@@ -50,12 +67,15 @@ class MainActivity : AppCompatActivity() {
             if (file.exists()) Files.delete(file.toPath())
             Files.createFile(file.toPath())
             Files.write(file.toPath(), dump)
-
-            val makeText = Toast.makeText(this, "Database has been dumped!", Toast.LENGTH_SHORT)
-            makeText.setGravity(Gravity.TOP, 0, dumpDb.top - 170)
-            makeText.show()
+            showToast("Database has been dumped!")
         }
     }
+
+    private fun showToast(msg: String) =
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).apply {
+            setGravity(Gravity.TOP, 0, dumpDb.top - 170)
+            show()
+        }
 
     fun toNotificationSettings(@Suppress("UNUSED_PARAMETER") view: View) =
         startActivity(Intent(this, NotificationSchedulingActivity::class.java))
