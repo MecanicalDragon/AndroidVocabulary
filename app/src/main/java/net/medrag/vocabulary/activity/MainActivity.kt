5d92b28f12cf0fd9d418366829847cee0d03f750
WildCoder <fg2946gznal30fu2>
2020-05-23 14:58:48 +0300

    version 1.1.2 - yandex API

--- app/src/main/java/net/medrag/vocabulary/activity/MainActivity.kt
+++ app/src/main/java/net/medrag/vocabulary/activity/MainActivity.kt
@@ -1,31 +1,45 @@
 package net.medrag.vocabulary.activity
 
+import android.content.ClipDescription.MIMETYPE_TEXT_HTML
 import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
 import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.medrag.vocabulary.R
 import net.medrag.vocabulary.db.ReducedPair
 import net.medrag.vocabulary.db.Repository
+import org.json.JSONArray
+import org.json.JSONObject
 import java.io.File
 import java.nio.file.Files
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var database: Repository
+    private lateinit var key: String
+    private lateinit var url: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         database = Repository(this)
+        key = resources.getString(R.string.yandexApiKey)
+        url = resources.getString(R.string.yandexApiRequestUri)
     }
 
     fun addToVoc(@Suppress("UNUSED_PARAMETER") view: View) {
@@ -43,15 +57,79 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun askYandex(@Suppress("UNUSED_PARAMETER") view: View) {
+        val word = wilEdit.text.toString()
+        val translation = tilEdit.text.toString()
+        if (word.isBlank() && translation.isBlank()) {
+            showToast("Nothing to ask.")
+            return
+        }
+        val text = if (word.isNotBlank()) word else translation
+        val mode = if (word.isNotBlank()) EN_RU else RU_EN
+        yandexRequest(text, mode)
+    }
+
+    private fun yandexRequest(textOut: String, mode: String) {
+        if (textOut.isBlank()) {
+            Log.e(MAIN_ACTIVITY, "Passed text is blank. There will be no request to Yandex.")
+            return
+        }
+        Log.i(MAIN_ACTIVITY, "Preparing request to Yandex...")
+        GlobalScope.launch(Dispatchers.Main) {
+            var data = ""
+            HttpClient().use {
+                data = it.get(String.format(YANDEX_REQUEST, url, key, textOut, mode))
+            }
+            Log.i(MAIN_ACTIVITY, "Request from yandex has been received: $data")
+            if (data.isBlank()) return@launch
+            val json = JSONObject(data)
+            val code = json.get(CODE) as Int
+            if (code == 200) {
+                val lang = json.get(LANG)
+                val resp = json.get(TEXT)
+                if (resp is JSONArray && resp.length() > 0) {
+                    when (lang) {
+                        RU_EN -> wilEdit.setText(resp[0].toString())
+                        EN_RU -> tilEdit.setText(resp[0].toString())
+                        else -> {
+                            Log.w(MAIN_ACTIVITY, "Unexpected response form Yandex: $lang")
+                        }
+                    }
+                } else {
+                    Log.e(MAIN_ACTIVITY, "Yandex request failed: $data")
+                }
+            } else {
+                Log.e(MAIN_ACTIVITY, "Yandex request failed: $data")
+            }
+        }
+    }
+
     fun pasteFromClipboardEn(@Suppress("UNUSED_PARAMETER") view: View) = pasteFromClipboard(wilEdit)
     fun pasteFromClipboardRu(@Suppress("UNUSED_PARAMETER") view: View) = pasteFromClipboard(tilEdit)
 
     private fun pasteFromClipboard(inputField: TextInputEditText) {
+        val mode = when (inputField) {
+            wilEdit -> EN_RU
+            tilEdit -> RU_EN
+            else -> {
+                Log.e(MAIN_ACTIVITY, "You've passed invalid field in the paste function")
+                showToast("WTF?! You've did smth, that breaks normal workflow of the app.")
+                return
+            }
+        }
         val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         if (clipboard.hasPrimaryClip()) {
             val clip = clipboard.primaryClipDescription
-            if (clip?.hasMimeType(MIMETYPE_TEXT_PLAIN) == true) {
-                inputField.setText(clipboard.primaryClip?.getItemAt(0)?.text)
+            if (clip?.hasMimeType(MIMETYPE_TEXT_PLAIN) == true
+                || clip?.hasMimeType(MIMETYPE_TEXT_HTML) == true
+            ) {
+                val text = clipboard.primaryClip?.getItemAt(0)?.text
+                if (text.isNullOrBlank()) {
+                    showToast("Text in clipboard is blank!")
+                } else {
+                    inputField.setText(text)
+                    yandexRequest(text.toString(), mode)
+                }
             } else showToast("Not a text in the clipboard!")
         } else showToast("The clipboard is empty!")
     }
@@ -86,5 +164,15 @@ class MainActivity : AppCompatActivity() {
         tilEdit.clearFocus()
         wilEdit.clearFocus()
         startActivity(Intent(this, LearningActivity::class.java))
+    }
+
+    companion object {
+        const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+        const val RU_EN = "ru-en"
+        const val EN_RU = "en-ru"
+        const val YANDEX_REQUEST = "%s?key=%s&text=%s&lang=%s&format=plain"
+        const val CODE = "code"
+        const val LANG = "lang"
+        const val TEXT = "text"
     }
 }
